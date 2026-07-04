710cd5bcdc3d37900194e1b6f78cdaab9b74fb19
Aishi De <aishibde@gmail.com>
2026-07-04 11:01:56 +0530

    Added more content

---
+++ Cpp_basics.cpp
@@ -0,0 +1,32 @@
+// AISHI DE 
+
+
+C++ is used to create computer programs, 
+and is one of the most used languages in game development.
+
+C++ was developed as an extension of C
+
+C++ is an object oriented language
+
+C++ is a cross-platform language : 
+A cross-platform language is a programming language that lets you write code once and run it on multiple operating systems with little or no changes.
+
+C++ was developed by Bjarne Stroustrup
+
+C++ can be found in today's operating systems, Graphical User Interfaces, and embedded systems.
+
+
+
+
+
+DIFFERENCE BETWEEN C and C++ 
+C++ supports classes and objects, while C does not.
+
+
+#include <iostream> is a header file library that lets us work with input and output objects, such as cout. Header files add functionality to C++ programs.
+
+using namespace std means that we can use names for objects and variables from the standard library.
+
+C++ is case-sensitive
+every statement end s with a semicolon ;
+
--- Hello_world.cpp
+++ Hello_world.cpp
@@ -7,3 +7,16 @@ int main() {
     cout << "Hello World I am Aishi !!!";
     return 0;
 }
+
+
+
+omitting the line namespace 
+
+
+
+#include <iostream>
+
+int main() {
+  std::cout << "Hello World!";
+  return 0;
+}
