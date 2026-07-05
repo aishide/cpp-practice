cf2796fe88a1cf648892cee406a5a64110f6558b
Aishi De <aishibde@gmail.com>
2026-07-05 23:30:05 +0530

    Added example of variable usgae in cpp

--- variables.cpp
+++ variables.cpp
@@ -14,3 +14,31 @@ bool
 syntax : 
 type variableName = value;
 
+
+
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int Mynum = 23;
+    cout << Mynum << endl;
+    Mynum = 78;
+    cout << Mynum << "\n\n";
+    
+    
+    
+double myFloatNum = 5.99;    
+char myLetter = 'D';     
+string myText = "Hello";     
+bool myBoolean = true; 
+    
+    
+    
+    int Age = 21;
+    cout << "My age is " << Age << " Years old!";
+    
+    return 0;
+}
+
