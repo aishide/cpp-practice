6a78159e5322a198f6cf468a18170fcc1691a27a
Aishi De <aishibde@gmail.com>
2026-07-06 23:14:05 +0530

    Written code of area od the rectangle

---
+++ area_of_rectangle.cpp
@@ -0,0 +1,16 @@
+// Aishi De 
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    //Area of a rectangle 
+    
+    float length = 2.33;
+    float breadth = 5.67;
+    
+    float area_of_rectangle = length * breadth;
+    
+    cout << "The area of the reactangle of length " << length << " and breadth " << breadth << " is : " << area_of_rectangle; 
+    return 0;
+}
