d61293a2f63c6bf9202f84b08fda8411ac900bd1
Aishi De <aishibde@gmail.com>
2026-07-07 23:09:11 +0530

    Added example to take user input

---
+++ user_input.cpp
@@ -0,0 +1,13 @@
+//Aishi De
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    int x;
+    cout << "Type a number : " << endl;
+    cin >> x;
+    cout << "Your Number is " << x;
+    return 0;
+}
