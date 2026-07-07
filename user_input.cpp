25be75e3d0a331a33a042955832d25047ee301d8
Aishi De <aishibde@gmail.com>
2026-07-07 23:36:51 +0530

    Addition of two numbers using input

--- user_input.cpp
+++ user_input.cpp
@@ -1,5 +1,8 @@
 //Aishi De
 
+
+//Number input 
+
 #include <iostream>
 using namespace std;
 
@@ -9,5 +12,24 @@ int main() {
     cout << "Type a number : " << endl;
     cin >> x;
     cout << "Your Number is " << x;
+    return 0;
+}
+
+
+// Addition of two numbers 
+
+#include <iostream>
+using namespace std;
+
+int main() {
+    
+    cout << "Addition of two numbers \n";
+    int x, y, sum ;
+    cout << "Type first number : " << endl;
+    cin >> x;
+    cout << "Type second number : " << endl;
+    cin >> y;
+    sum = x + y;
+    cout << "the sum of " << x << " and " << y << " is : " << sum ;
     return 0;
 }
