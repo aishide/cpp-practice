ff20b6dca2b593ebe1d381aa8a30d42f2d426f2d
Aishi De <aishibde@gmail.com>
2026-07-09 21:13:15 +0530

    Added the theory and examples of auto keyword

---
+++ auto_keyword.cpp
@@ -0,0 +1,12 @@
+// AISHI 
+
+The auto keyword automatically detects the type of a variable based on the value you assign to it.
+
+
+auto x = 7;
+
+
+
+auto only works when you assign a value at the same time (You cant declare auto x; without assigning a value)
+
+Once the type is chosen, it stays the same. 
