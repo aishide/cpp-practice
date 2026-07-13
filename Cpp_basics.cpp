88c9670a20e4bbcb4bcbffd29dd78055cb14df13
Aishi De <aishibde@gmail.com>
2026-07-13 19:21:45 +0530

    Added cmath header use

--- Cpp_basics.cpp
+++ Cpp_basics.cpp
@@ -202,3 +202,16 @@ cpp maths  :
 
 cout << max(5, 10);
 cout << min(5, 10);
+
+Other functions, such as sqrt (square root), round (rounds a number) and log (natural logarithm), can be found in the <cmath> header file:
+
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main() {
+  cout << "Square root of 64 is: " << sqrt(64) << "\n";
+  cout << "Round 2.6 to the nearest integer: " << round(2.6) << "\n";
+  cout << "The natural logarithm of 2 is: " <<log(2) << "\n";
+  return 0;
+}
