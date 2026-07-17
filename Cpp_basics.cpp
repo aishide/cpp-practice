6861aedef870b3be62b962658a0fcec13a66c694
Aishi De <aishibde@gmail.com>
2026-07-17 23:31:32 +0530

    Added code with omitting namespace std

--- Cpp_basics.cpp
+++ Cpp_basics.cpp
@@ -311,6 +311,16 @@ int main() {
 }
 
 
+omitting namespace std : 
+#include <iostream>
+#include <string>
+// using namespace std; - Remove this line
+
+int main() {
+  std::string greeting = "Hello";
+  std::cout << greeting;
+  return 0;
+}
 
 
 
