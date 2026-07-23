a94dd01c72dd32b8ac5badb1281e01da7f433def
Aishi De <aishibde@gmail.com>
2026-07-23 19:19:04 +0530

    storing boolean values in a variable

--- Cpp_basics.cpp
+++ Cpp_basics.cpp
@@ -408,3 +408,14 @@ int x = 10;
 cout << (x == 10);  // returns 1 (true), because the value of x is equal to 10
 
 cout << (10 == 15);  // returns 0 (false), because 10 is not equal to 15
+
+
+
+storing boolean values in variables :
+
+int x = 10;
+int y = 9;
+
+bool isGreater = x > y;
+
+cout << isGreater; // returns 1 (true)
