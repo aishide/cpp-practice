0c59534aaa3ee5a41bdeaf89d5682bfa02ad64e7
Aishi De <aishibde@gmail.com>
2026-07-23 19:17:44 +0530

    Added equal operator boolean result

--- Cpp_basics.cpp
+++ Cpp_basics.cpp
@@ -403,3 +403,7 @@ int x = 10;
 int y = 9;
 cout << (x > y); // returns 1 (true), because 10 is higher than 9
 
+
+int x = 10;
+cout << (x == 10);  // returns 1 (true), because the value of x is equal to 10
+
