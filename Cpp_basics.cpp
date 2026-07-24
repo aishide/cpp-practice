f1e82cec83a9b137647845fc2553082f13193e8b
Aishi De <aishibde@gmail.com>
2026-07-25 00:24:48 +0530

    Added age and boolean example

--- Cpp_basics.cpp
+++ Cpp_basics.cpp
@@ -419,3 +419,14 @@ int y = 9;
 bool isGreater = x > y;
 
 cout << isGreater; // returns 1 (true)
+
+
+age example :
+int myAge = 25;
+int votingAge = 18;
+
+cout << (myAge >= votingAge); // returns 1 (true), meaning 25 year olds are allowed to vote!
+
+
+
+
