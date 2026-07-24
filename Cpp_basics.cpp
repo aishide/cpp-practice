cbc10a5668f0e4b5bf049a6fd22aa1b521df39d0
Aishi De <aishibde@gmail.com>
2026-07-25 00:25:34 +0530

    Added if and else example

--- Cpp_basics.cpp
+++ Cpp_basics.cpp
@@ -430,3 +430,17 @@ cout << (myAge >= votingAge); // returns 1 (true), meaning 25 year olds are allo
 
 
 
+if else  : 
+
+int myAge = 25;
+int votingAge = 18;
+
+if (myAge >= votingAge) {
+  cout << "Old enough to vote!";
+} else {
+  cout << "Not old enough to vote.";
+}
+
+// Outputs: Old enough to vote!
+
+
