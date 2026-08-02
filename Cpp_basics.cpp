3ff56d81b4705f085bede095c4af3b5ee5ed13d6
Aishi De <aishibde@gmail.com>
2026-08-02 23:58:45 +0530

    Switch Case Example in cpp

--- Cpp_basics.cpp
+++ Cpp_basics.cpp
@@ -551,3 +551,32 @@ switch(expression) {
   default:
     // code block
 }
+
+Example :
+
+int day = 4;
+switch (day) {
+  case 1:
+    cout << "Monday";
+    break;
+  case 2:
+    cout << "Tuesday";
+    break;
+  case 3:
+    cout << "Wednesday";
+    break;
+  case 4:
+    cout << "Thursday";
+    break;
+  case 5:
+    cout << "Friday";
+    break;
+  case 6:
+    cout << "Saturday";
+    break;
+  case 7:
+    cout << "Sunday";
+    break;
+}
+// Outputs "Thursday" (day 4)
+
