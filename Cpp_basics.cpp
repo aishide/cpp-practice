c57a06230c79a52b729fe66d4a52a56ae7afa6e4
Aishi De <aishibde@gmail.com>
2026-08-05 20:50:15 +0530

    Added second example of Nested for loop

--- Cpp_basics.cpp
+++ Cpp_basics.cpp
@@ -634,3 +634,17 @@ Outer: 2
  Inner: 1
  Inner: 2
  Inner: 3
+
+
+ Example : 
+ for (int i = 1; i <= 3; i++) {
+  for (int j = 1; j <= 3; j++) {
+    cout << i * j << " ";
+  }
+  cout << "\n";
+}
+
+Output : 
+1 2 3
+2 4 6
+3 6 9
