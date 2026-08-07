cc8d2acb29867a5ecd0df996f26206aff4b3132a
Aishi De <aishibde@gmail.com>
2026-08-07 17:16:20 +0530

    Save current changes

--- Cpp_basics.cpp
+++ Cpp_basics.cpp
@@ -659,3 +659,14 @@ for (int i = 0; i < 10; i++) {
   }
   cout << i << "\n";
 }
+
+//Continue statememt :
+The continue statement breaks one iteration (in the loop), if a specified condition occurs, and continues with the next iteration in the loop.
+This example skips the value of 4:
+
+for (int i = 0; i < 10; i++) {
+  if (i == 4) {
+    continue;
+  }
+  cout << i << "\n";
+}
