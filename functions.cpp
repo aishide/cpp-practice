010f890e40393f213d3330b773f0d86a989da2af
Aishi De <149359864+aishide@users.noreply.github.com>
2026-09-04 23:54:10 +0530

    Add comment about pre-defined functions in C++

--- functions.cpp
+++ functions.cpp
@@ -2,3 +2,4 @@
 // A function is a block of code which only runs when it is called.
 // You can pass data, known as parameters, into a function.
 
+// C++ provides some pre-defined functions, such as main(), which is used to execute code. 
