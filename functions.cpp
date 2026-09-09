32b4465889b5a529ca696871bbcc4d864c13a42d
Aishi De <149359864+aishide@users.noreply.github.com>
2026-09-09 23:47:02 +0530

    Implement myFunction to print execution message
    Added a function that prints a message to the console.

--- functions.cpp
+++ functions.cpp
@@ -22,3 +22,25 @@ int main() {
 }
 
 // Outputs "I just got executed!"
+
+
+
+
+..
+
+// We can call it multiple times too!
+
+void myFunction() {
+  cout << "I just got executed!\n";
+}
+
+int main() {
+  myFunction();
+  myFunction();
+  myFunction();
+  return 0;
+}
+
+// I just got executed!
+// I just got executed!
+// I just got executed!
