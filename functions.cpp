f6331bd9e7cb8f9cd3bdf424ba8232123fd412a8
Aishi De <149359864+aishide@users.noreply.github.com>
2026-09-11 18:09:59 +0530

    Update functions.cpp

--- functions.cpp
+++ functions.cpp
@@ -53,3 +53,27 @@ int main() {
 void functionName(parameter1, parameter2, parameter3) {
   // code to be executed
 }
+
+
+
+
+
+//Name function 
+
+void myFunction(string fname) {
+  cout << fname << " Refsnes\n";
+}
+
+int main() {
+  myFunction("Liam");
+  myFunction("Jenny");
+  myFunction("Anja");
+  return 0;
+}
+
+// Liam Refsnes
+// Jenny Refsnes
+// Anja Refsnes
+
+
+
