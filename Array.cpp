18fed880d638550662ec54495313554e5f8b5ecb
Aishi De <149359864+aishide@users.noreply.github.com>
2026-09-13 18:23:35 +0530

    Add comments for array declaration and usage
    Added comments explaining array usage and declaration examples.

--- Array.cpp
+++ Array.cpp
@@ -1,1 +1,7 @@
+// Arrays are used to store multiple values in a single variable, instead of declaring separate variables for each value.
+//To declare an array, define the variable type, specify the name of the array followed by square brackets and specify the number of elements it should store
 
+Example : 
+
+string cars[4] = {"Volvo", "BMW", "Ford", "Mazda"};
+int myNum[3] = {10, 20, 30};
